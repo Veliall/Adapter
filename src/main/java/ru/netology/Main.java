b4c2174e62b852140934a7960b7886c3d7198c7f
Igor Khristiuk <fenix-kanev@yandex.ru>
2021-07-29 20:05:03 +0300

    initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+/**
+ * @author Igor Khristiuk
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+    }
+}
